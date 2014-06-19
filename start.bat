af79d1a96b2698a9f94706684c8f45db55c6744b
XIRUIBIN <XIRUIBIN@XIRUIBIN-NB-PC>
2014-06-20 00:12:08 +0800

    增加db_doc.exe

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
-#java -Djava.ext.dirs=lib; -cp ./db_doc.jar com.xiruibin.CMDTool
-java -Djava.ext.dirs=lib; -cp ./db_doc.jar com.xiruibin.CMDTool -h 192.168.1.170 -d v6estore -u estore -p estore -P 50001 -s estore --path d:\\
+::java -Djava.ext.dirs=lib; -cp ./db_doc.jar com.xiruibin.CMDTool
+::java -Djava.ext.dirs=lib; -cp ./db_doc.jar com.xiruibin.CMDTool -h 192.168.1.170 -d v6estore -u estore -p estore -P 50001 -s estore --path d:\\
+db_doc -h 192.168.1.170 -d v6estore -u estore -p estore -P 50001 -s estore --path d:\\
 pause
