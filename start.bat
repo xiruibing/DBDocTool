87b5af9e2c428d8140c2808c5317dbd3bf800f5e
xbinbin <xiruibing@126.com>
2015-02-27 11:16:10 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-::java -Djava.ext.dirs=lib; -cp ./db_doc.jar com.xiruibin.CMDTool
-::java -Djava.ext.dirs=lib; -cp ./db_doc.jar com.xiruibin.CMDTool -h 192.168.1.170 -d v6estore -u estore -p estore -P 50001 -s estore --path d:\\
-db_doc -h 192.168.1.170 -d v6estore -u estore -p estore -P 50001 -s estore --path d:\\
+color 3f
+title DBDocTool
+db_doc -h 192.168.1.170 -d dbtest -u root -p root -P 3306 --path d:\\
 pause
